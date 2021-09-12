c213e324a6a8f39a3c81e3125e94509d455db94e
damadape <danielsapien@gmail.com>
2021-09-12 13:39:12 -0500

    added loc_gen

--- Img/imglib.py
+++ Img/imglib.py
@@ -3,13 +3,15 @@ import skfuzzy as fuzzy
 from skimage import color
 
 
-def histograma(img, res=255):
-    # Histograma normalizado de imagen
+def histograma(img_in, res=255):
+    # Histograma normalizado de imagen de un canal
+    scale = res/max(img_in.flatten())
+    img = np.rint(scale*img_in)
     hist = np.zeros(res+1)
-    scale = res/max(img.flatten())
+    
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            idx = int(scale*img[i,j])
+            idx = int(img[i,j])
             hist[idx] += 1
     return hist/sum(hist)
 
@@ -21,7 +23,7 @@ def acumulado(xs):
 
 def ecu_hist(img, res=255):
     """
-    Ecualización de histograma
+    Ecualización de histograma de un sólo canal
     res: Subdivisiones del histograma
     """
     acu = acumulado(histograma(img, res=res))
@@ -38,6 +40,8 @@ def ecu_hist(img, res=255):
 def ecu_fuzzy(img, singl, fuzzy_sets, res=255):
     """
     Ecualización difusa de histograma
+    singl: vector de singletons de conjuntos
+    fuzzy_sets: Funciones de membresía por conjunto
     res: Subdivisiones del histograma
     """
     ecufuzz = np.zeros(res+1)
@@ -63,3 +67,32 @@ def trans_v(img_rgb, trans):
     v_trans = trans(np.copy(img_hsv[:,:,2]))
     img_hsv[:,:,2] = v_trans
     return color.hsv2rgb(img_hsv)
+
+
+def loc_gen(img, step: int):
+    """
+    Generador de secciones cuadradas para imagen de 1 canal
+    Genera rebanadas np._s para usarlos como img[loc]
+    shape: Tamaño total a subdividir
+    step: Lado de sección cuadrada
+    """
+    fil = int(img.shape[0]/step)
+    col = int(img.shape[1]/step)
+
+    for f in range(fil-1):
+        for c in range(col-1):
+            yield np.s_[step*f: step*(f+1), step*c: step*(c+1)]
+
+    #Bloques al margen cubren el sobrante del paso
+    ext_f = int(img.shape[0])%step
+    ext_c = int(img.shape[1])%step
+    #Recorrido de bloques al margen derecho
+    for f in range(fil-1):
+        yield np.s_[step*f: step*(f+1), -(step + ext_c): ]
+
+    #Recorrido de bloques al margen inferior
+    for c in range(col-1):
+        yield np.s_[-(step + ext_f): , step*c: step*(c+1)]
+
+    #Esquina inferior derecha, caso mínimo
+    yield np.s_[-(step + ext_f): , -(step + ext_c): ]
