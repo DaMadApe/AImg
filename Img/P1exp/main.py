59e8ab323ec8a0b038fae5862d774a6aea39af5d
damadape <danielsapien@gmail.com>
2021-09-30 03:03:58 -0500

    popup de filechooser funcional

--- Img/P1exp/main.py
+++ Img/P1exp/main.py
@@ -3,7 +3,9 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
@@ -17,13 +19,14 @@ class MainContainer(BoxLayout):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.load_file_popup = Load_file_popup(load=self.load)
+        self.img = None
+        self.proc = None
 
     def slideCompare(self, instance, val):
         self.ids.sliderVal.text = "% d"% val
 
-    def cargarImagen(self):
-        img = cv2.imread('datos/panda.jpg', cv2.IMREAD_UNCHANGED)
+    def cargarImagen(self, path):
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.flip(img, 0)
         img_eq = np.ones(img.shape, dtype=np.uint8)
@@ -32,23 +35,28 @@ class MainContainer(BoxLayout):
         imgView = Texture.create(size=(w, h))
         imgView.blit_buffer(img_canvas.flatten(), colorfmt='rgb', bufferfmt='ubyte')
 
-    def callback_load(self, *args):
+    def load_popup(self, *args):
+        self.load_file_popup = Load_file_popup(load=self.load)
         self.load_file_popup.open()
 
     def load(self, selection):
         self.file_path = str(selection[0])
         self.ids.pathLabel.text = self.file_path
-        self.dismiss()
+        self.load_file_popup.dismiss()
+
+
+
+
+class Load_file_popup(Popup):
+    load = ObjectProperty()
 
 
 class BetterImgApp(App):
     def build(self):
         container = MainContainer()
+        #container.cargarImagen()
         return container
 
-
-class Load_file_popup(Popup):
-    load = ObjectProperty()
 
 
 if __name__ == '__main__':
