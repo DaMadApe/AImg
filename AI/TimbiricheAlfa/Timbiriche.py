1e962b0219551015f5867708de54c2b161b87150
damadape <danielsapien@gmail.com>
2021-10-03 01:57:22 -0500

    Simetrías implementadas

--- AI/TimbiricheAlfa/Timbiriche.py
+++ AI/TimbiricheAlfa/Timbiriche.py
@@ -1,27 +1,125 @@
-import os
 import numpy as np
-import torch
 
 from utils import *
 from Game import Game
-from TimbiricheLogic import Tablero
-from NeuralNet import NeuralNet
-from TimbiricheNet import TimbiricheNet
 
-args = dotdict({
-    'lr': 0.001,
-    'dropout': 0.3,
-    'epochs': 10,
-    'batch_size': 64,
-    'cuda': torch.cuda.is_available(),
-    'num_channels': 512,
-})
+"""
+Este archivo contiene la definición de la lógica de juego
+con la clase Tablero y la definición de la interfaz que
+define las interacciones entre el proceso de entrenamiento
+y el juego en el tablero.
+"""
+
+
+class Tablero():
+    """
+    Representación del tablero de juego, incluyendo métodos
+    para tirar en el tablero y validar los movimientos posibles.
+    """
+
+    def __init__(self, n=5):
+        self.tablero = np.zeros((n, n), dtype=np.uint8)
+        self.n = n
+        # Conjunto de acciones, para tirar con un solo entero
+        self.actions = []         
+        for y in range(self.n):
+            for x in range(self.n):
+                self.actions.append((y,x,1))
+                self.actions.append((y,x,2))
+
+    def __getitem__(self, idx):
+        return self.tablero[idx]
+
+    def __setitem__(self, idx, val):
+        self.tablero[idx] = val
+
+    def __repr__(self):
+        # Para definir lo que devuelve print(self)
+        # Se invierte eje vertical al imprimir por conveniencia
+        repr = '\n '
+        for i in range(self.n):
+            repr += str(i) + ' '
+        sym = ['. ', '| ', '._', '|_']
+        for j, fil in enumerate(self.tablero):
+            for pos in fil[::-1]:
+                repr = sym[pos] + repr
+            repr = f'\n{j}' + repr
+        return repr
+
+    def validarTiro(self, y, x, mov):
+        pos = self[y, x]
+        # Validación de tiro
+        cond = mov in [1, 2]
+        if x < self.n-1 and y < self.n-1:
+            cond &= (pos == 0 or (pos+mov == 3))
+        else:
+            # Puntos en la orilla del tablero están restringidos
+            if x == self.n-1:
+                cond &= mov == 1  # Sólo se vale ir vertical
+            if y == self.n-1:
+                cond &= mov == 2  # Sólo se vale ir horizontal
+        return cond
+
+    @staticmethod
+    def validarExterno(tablero, y, x, mov):
+        n = tablero.shape[0]
+        pos = tablero[y, x]
+        # Validación de tiro
+        cond = mov in [1, 2]
+        if x < n-1 and y < n-1:
+            cond &= (pos == 0 or (pos+mov == 3))
+        else:
+            # Puntos en la orilla del tablero están restringidos
+            if x == n-1:
+                cond &= mov == 1  # Sólo se vale ir vertical
+            if y == n-1:
+                cond &= mov == 2  # Sólo se vale ir horizontal
+        return cond
+
+    def mover(self, y, x, mov):
+        """
+        x: Columna
+        y: Fila
+        mov: Tiro, 1 para vertical, 2 para horizontal 
+        """
+        if self.validarTiro(y, x, mov):
+            self.tablero[y, x] += mov
+            p = self.evalScore(y, x, mov)
+            return 1 + p  # Turno completo + los puntos ganados
+        return 0  # Turno incompleto
+
+    def evalScore(self, y, x, mov):
+        """
+        Calcular puntaje correspondiente a un tiro (y,x,mov)
+        Revisar si la línea cierra algún cuadrado
+        """
+        p = 0
+        if mov == 1:
+            # pos%2 = (pos==1 or pos==3)
+            # pos>1 = (pos==2 or pos==3)
+            if self[y, x-1] == 3 and self[y+1, x-1] > 1:
+                p += 1
+            if self[y, x] == 3 and self[y, x+1] > 1 and self[y, x+1] % 2:
+                p += 1
+        else:
+            if self[y, x-1] == 3 and self[y-1, x+1] % 2:
+                p += 1
+            if self[y, x] == 3 and self[y, x+1] % 2 and self[y, x+1] > 1:
+                p += 1
+        return p
 
 
 class Timbiriche(Game):
 
     def __init__(self, n):
-        self.n
+        self.n = n
+        self.score = [0, 0]
+        # Conjunto de acciones, para tirar con un solo entero
+        self.actions = []         
+        for y in range(self.n):
+            for x in range(self.n):
+                self.actions.append((y,x,1))
+                self.actions.append((y,x,2))
 
     def getInitBoard(self):
         """
@@ -29,23 +127,21 @@ class Timbiriche(Game):
             startBoard: a representation of the board (ideally this is the form
                         that will be the input to your neural network)
         """
-        tablero = np.zeros((self.n, self.n), dtype=np.uint8)
-        return tablero
+        return Tablero(self.n).tablero
 
     def getBoardSize(self):
         """
         Returns:
             (x,y): a tuple of board dimensions
         """
-        return (self.n, self.n)
+        return (self.n+1, self.n)
 
     def getActionSize(self):
         """
         Returns:
             actionSize: number of all possible actions
         """
-        size = 2*(self.n-1)**2 + 2*self.n
-        return size
+        return 2*self.n**2
 
     def getNextState(self, board, player, action):
         """
@@ -60,11 +156,13 @@ class Timbiriche(Game):
         """
         nextBoard = Tablero(self.n)
         nextBoard.tablero = np.copy(board)
-        p = nextBoard.move(action)
+        p = nextBoard.move(self.actions[action])
         if p==1: # Tiro válido sin anotaciones
             nextPlayer = -player
-        else:
+        else: # Tiro fallido o tiro con anotación
             nextPlayer = player
+        if p>1: # Anotación
+            self.score[(player+1)//2] += p-1
         return nextBoard.tablero, nextPlayer
 
     def getValidMoves(self, board, player):
@@ -78,14 +176,10 @@ class Timbiriche(Game):
                         moves that are valid from the current board and player,
                         0 for invalid moves
         """
-        tablero = Tablero(self.n)
-        tablero.tablero = np.copy(board)
         validMoves = []
-        for y in range(self.n):
-            for x in range(self.n):
-                validMoves.append(board.validar(y,x,1))
-                validMoves.append(board.validar(y,x,2))
-
+        for action in self.actions:
+            is_valid = Tablero.validarExterno(board, *action)
+            validMoves.append(int(is_valid))
         return validMoves
 
     def getGameEnded(self, board, player):
@@ -98,9 +192,16 @@ class Timbiriche(Game):
             r: 0 if game has not ended. 1 if player won, -1 if player lost,
                small non-zero value for draw.
         """
-        tablero_lleno = 3*(self.n-1)**2 + 3*(self.n-1)
-        r = np.sum(board) == tablero_lleno
-        return r
+        s1, s2 = self.score
+        if s1+s2 == self.n**2:
+            if s1>s2:
+                return player
+            if s2>s1:
+                return -player
+            else:
+                return 0.001 # Empate
+        else:
+            return 0 
 
     def getCanonicalForm(self, board, player):
         """
@@ -129,8 +230,77 @@ class Timbiriche(Game):
                        form of the board and the corresponding pi vector. This
                        is used when training the neural network from examples.
         """
+        symts = []
 
-        pass
+        def rot90(tab, n):
+            # Rotación de tablero +90 según mano derecha
+            rot_tab = Tablero(n)
+            for y in range(n):
+                for x in range(n):
+                    prev = tab[y,x]
+                    if prev == 1:
+                        rot_tab[x,n-y-2] += 2
+                    if prev == 2:
+                        rot_tab[x,n-y-1] += 1
+                    if prev == 3:
+                        rot_tab[x,n-y-2] += 2
+                        rot_tab[x,n-y-1] += 1
+            return rot_tab.tablero
+
+        def rot_pol90(pol, n):
+            # Rotación correspondiente del vector pi
+            polmat = np.reshape(pol, (n, n, 2))
+            newmat = np.zeros((n, n, 2))
+            for i in range(n):
+                for j in range(n):
+                    newmat[n-j-1, i, 1] = polmat[i, j, 0]
+                    newmat[n-j-2, i, 0] = polmat[i, j, 1]
+            return newmat.flatten()
+
+        def reflect(tab, n):
+            # Reflejar respecto eje x
+            ref_tab = Tablero(n)
+            for y in range(n):
+                for x in range(n):
+                    prev = tab[y,x]
+                    if prev == 1:
+                        ref_tab[n-y-2, x] += 1
+                    if prev == 2:
+                        ref_tab[n-y-1, x] += 2
+                    if prev == 3:
+                        ref_tab[n-y-2, x] += 1
+                        ref_tab[n-y-1, x] += 2
+            return ref_tab.tablero
+
+        def reflect_pol(pol, n):
+            # Reflexión correspondiente de vector pi
+            polmat = np.reshape(pol, (n, n, 2))
+            newmat = np.zeros((n, n, 2))
+            for i in range(n):
+                for j in range(n):
+                    newmat[n-i-2, j, 0] = polmat[i, j, 0]
+                    newmat[n-i-1, j, 1] = polmat[i, j, 1]
+            return newmat.flatten()
+
+        tab_rot90 = rot90(board, self.n)
+        tab_rot180 = rot90(tab_rot90, self.n)
+        tab_rot270 = rot90(tab_rot180, self.n)
+
+        pi_rot90 = rot_pol90(pi, self.n)
+        pi_rot180 = rot_pol90(pi_rot90, self.n)
+        pi_rot270 = rot_pol90(pi_rot180, self.n)
+
+        tab_ref1 = reflect(board, self.n)
+        tab_ref2 = reflect(tab_rot90, self.n)
+
+        pi_ref1 = reflect_pol(pi, self.n)
+        pi_ref2 = reflect_pol(pi_rot90, self.n)
+
+        return [(tab_rot90, pi_rot90),
+                (tab_rot180, pi_rot180),
+                (tab_rot270, pi_rot270),
+                (tab_ref1, pi_ref1),
+                (tab_ref2, pi_ref2)]
 
     def stringRepresentation(self, board):
         """
@@ -141,67 +311,4 @@ class Timbiriche(Game):
             boardString: a quick conversion of board to a string format.
                          Required by MCTS for hashing.
         """
-        return board.toString()
-
-
-class ModeloNeuronal(NeuralNet):
-    """
-    Este es un envoltorio de la clase TimbiricheNet, que
-    sirve de interfaz entre el modelo y el resto del
-    programa. Esta clase sigue los prototipos de
-    la clase NeuralNet, y de ahí vienen
-    las siguientes descripciones
-    """
-    def __init__(self, game):
-        self.net = TimbiricheNet
-
-    def train(self, examples):
-        """
-        This function trains the neural network with examples obtained from
-        self-play.
-
-        Input:
-            examples: a list of training examples, where each example is of form
-                      (board, pi, v). pi is the MCTS informed policy vector for
-                      the given board, and v is its value. The examples has
-                      board in its canonical form.
-        """
-        self.net.train()
-
-    def predict(self, board):
-        """
-        Input:
-            board: current board in its canonical form.
-
-        Returns:
-            pi: a policy vector for the current board- a numpy array of length
-                game.getActionSize
-            v: a float in [-1,1] that gives the value of the current board
-        """
-        self.net.predict(board)
-
-    def save_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
-        """
-        Saves the current neural network (with its parameters) in
-        folder/filename
-        """
-        filepath = os.path.join(folder, filename)
-        if not os.path.exists(folder):
-            print("Checkpoint Directory does not exist! Making directory {}".format(folder))
-            os.mkdir(folder)
-        else:
-            print("Checkpoint Directory exists! ")
-        torch.save({
-            'state_dict': self.nnet.state_dict(),
-        }, filepath)
-
-    def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
-        """
-        Loads parameters of the neural network from folder/filename
-        """
-        filepath = os.path.join(folder, filename)
-        if not os.path.exists(filepath):
-            raise ("No model in path {}".format(filepath))
-        map_location = None if args.cuda else 'cpu'
-        checkpoint = torch.load(filepath, map_location=map_location)
-        self.net.load_state_dict(checkpoint['state_dict'])
+        return np.array_str(board)
