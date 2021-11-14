58141177f9365ab7b14823a61ad56e96976b89ab
damadape <danielsapien@gmail.com>
2021-11-14 10:32:00 -0600

    Reorganizado para experimentar

--- Img/Segundo parcial/binVascular.py
+++ Img/Segundo parcial/binVascular.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import (io, data, exposure, util,
                      filters, morphology)
+from skimage.morphology import disk
+from skimage.filters import rank 
 
 
 def pasabajos_ideal(shape, f_corte):
@@ -24,69 +26,54 @@ def aplicar_filtro_frec(img, kernel):
         filtro[:kernel.shape[0], :kernel.shape[1]] = kernel
 
     img_frec = np.fft.fftshift(np.fft.fft2(img))
-    filtro = pasaaltos_ideal(img_g.shape, 100)
+    filtro = pasaaltos_ideal(img.shape, 100)
 
     # Aplicar el filtro en frecuencia
     convolucion = img_frec * filtro
     # Regresar al dominio espacial
     img_ift = np.fft.ifft2(np.fft.fftshift(convolucion))
-    # Acondicionar como imagen visible
-    # venas = np.array(np.abs(img_ift), dtype=np.ubyte)
-    return np.abs(img_ift)
+    img_final = np.abs(img_ift)
+    img_final = util.img_as_ubyte(img_final/np.max(img_final))
+    return img_final
 
 
+# Cargar imagen
 img = io.imread('datos/ojo/03_L.jpg')
 #img = data.retina()
-img_g = img[:,:,1]
-
-img_eq = util.img_as_ubyte(exposure.equalize_adapthist(img_g))
-#img_eq = util.img_as_ubyte(filters.rank.equalize(img_g, morphology.disk(5)))
-
-img_bin = util.img_as_ubyte(img_eq > filters.rank.otsu(img_eq, morphology.disk(30)))
 
 
-img_med = filters.rank.median(img_bin, morphology.disk(15))
+# Pasos del procesamiento
+proceso = []
 
-img_final = img_med > filters.threshold_otsu(img_med)
+img_proc = img[:,:,1]
+proceso.append((img_proc.copy(), 'Canal Verde Aislado'))
 
+img_proc = util.img_as_ubyte(exposure.equalize_adapthist(img_proc))
+proceso.append((img_proc.copy(), 'Ecualización apaptativa'))
 
-"""
-Procesar en frecuencia
-"""
-filtro = pasabajos_ideal(img_bin.shape, 100)
-venas = aplicar_filtro_frec(img_bin, filtro)
+img_proc = util.img_as_ubyte(img_proc < rank.otsu(img_proc, disk(30)))
+proceso.append((img_proc.copy(), 'Umbral localizado de Otsu'))
+
+img_proc = rank.median(img_proc, disk(15))
+proceso.append((img_proc.copy(), 'med mediano'))
+
+#img_final = img_med > filters.threshold_otsu(img_med)
+img_proc = morphology.opening(img_proc/255, disk(15))
+proceso.append((img_proc.copy(), 'Opening a imagen umbral'))
+
 
 # Mostrar imágenes
+plt.figure(figsize=(15, 8))
 plt.subplot(2,3,1)
 plt.imshow(img)
-plt.title('1. Imagen original')
-
-plt.subplot(2,3,2)
-plt.imshow(img_g, cmap='gray')
-plt.title('2. Canal verde aislado')
-
-plt.subplot(2,3,3)
-plt.imshow(img_eq, cmap='gray')
-plt.title('3. Ecualización adaptativa')
-
-plt.subplot(2,3,4)
-plt.imshow(img_bin, cmap='gray')
-plt.title('4. Umbral Otsu local')
-
-plt.subplot(2,3,5)
-plt.imshow(img_med, cmap='gray')
-plt.title('5. Filtro mediano')
-
-plt.subplot(2,3,6)
-plt.imshow(img_final, cmap='gray')
-plt.title('6. Umbral Otsu')
-
 plt.axis('off')
+plt.title('Imagen original')
+
+for i, (img, titulo) in enumerate(proceso):
+    plt.subplot(2,3,i+2)
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+    plt.title(f'{i+1}. {titulo}')
+
 plt.tight_layout()
 plt.show()
-
-# from crecimientoReg import (registrar_semillas,
-#                             aislar_region)
-# # Método crecimiento de región
-# seed = registrar_semillas(img_bin2)
-# reg = aislar_region(img_bin2, seed, tolerancia=0.15, visual=False)
