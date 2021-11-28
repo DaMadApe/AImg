b732f1fd3f82587c9bf95918ee2519e679c932ea
damadape <danielsapien@gmail.com>
2021-11-28 01:18:02 -0600

    Reorganización

--- Img/Tercer parcial/reconst3d.py
+++ Img/Tercer parcial/reconst3d.py
@@ -5,55 +5,53 @@ from skimage import (io, data, util, color,
 from skimage.filters import rank
 from scipy.spatial.transform import Rotation as R
 
-"""
-Obtener contornos de cada imagen
-Muestrear ~100 puntos del contorno
-Guardar con el ángulo codificado
-"""
-rot = np.pi/46
-angles = [rot*i for i in range(99, 202)]
-contornos = []
+from rayosParalelos import *
 
-for indx in range(99, 202):
+"""
+Procesar las imagenes de proyección y calcular transformación
+de Radón para las rebanadas normales al eje de giro
+"""
+idx_inicio = 104 # Número de primera imagen (voltea a derecha)
+idx_final = 150 # Número de última imagen (voltea a izquierda)
+n_imgs = idx_final - idx_inicio +1
+# Recorte de las imágenes
+y0, y1 = 270, 830
+x0, x1 = 10, 700
+recorte = np.s_[y0:y1, x0:x1]
+h_rebanada = 30
+rebanadas_radon = np.zeros(((y1-y0)//h_rebanada, n_imgs, x1-x0))
+
+for idx in range(0, n_imgs):
     # Cargar imagen
-    img = io.imread(f'datos/craneo_3d/IMG_0745 ({indx}).jpg')
-
+    img = io.imread(f'datos/craneo_3d/IMG_0745 ({idx+idx_inicio}).jpg')
     # Monocromatización y recorte manual
     img_proc = util.img_as_ubyte(color.rgb2gray(img))
-    img_proc = img_proc[250:850]
+    img_proc = img_proc[recorte]
     # Filtro mediano
     img_proc = rank.median(img_proc, morphology.disk(8))
     # Umbral de Otsu
     img_proc = util.img_as_ubyte(img_proc < filters.threshold_otsu(img_proc))
 
-    borde = morphology.binary_dilation(img_proc) & ~img_proc
-
-    puntos = np.argwhere(borde) # ~1500 puntos
-    puntos = puntos[::15] # Tomar ~100 puntos por contorno
-
-    contornos.append(puntos)
+    # Sumar las rebanadas en este ciclo ahorra la necesidad
+    # de tener todas las imágenes en memoria
+    for i in range(rebanadas_radon.shape[0]):
+        filas = np.s_[h_rebanada*i:h_rebanada*(i+1)]
+        rebanada = np.average(img_proc[filas], axis=0)
+        rebanadas_radon[i, idx] += rebanada
 
 """
-Rotar conjuntos de puntos
+Procesar el sinograma de cada rebanada
 """
-
-nube_puntos = np.array([[0,0,0]])
-for i, puntos in enumerate(contornos):
-    # Agregar valor z a cada coordenada
-    n = puntos.shape[0]
-    puntos = np.concatenate((puntos, np.zeros((n,1))), axis=1)
-
-    # Generar matriz de rotación en torno a x, y desplazamientos
-    rot = R.from_rotvec((angles[i], 0, 0)).as_matrix()
-    tr1 = np.array([0, -320, 0])
-    tr2 = np.array([0, -320, 0])
-    # Rotar y desplazar cada punto de los contornos
-    for punto in range(n):
-        puntos[punto] = np.dot(rot, puntos[punto]+tr1) # +tr2
-
-    nube_puntos = np.concatenate((nube_puntos, puntos), axis=0)
+recons = np.zeros((y1-y0, x1-x0, x1-x0))
+for i, rebanada in enumerate(rebanadas_radon):
+    proc = filtro_hamming(rebanada)
+    proc = inv_radon(proc)
+    filas = np.s_[h_rebanada*i:h_rebanada*(i+1)]
+    recons[filas] = proc * np.ones((h_rebanada,1,1))
 
 
-ax = plt.axes(projection='3d')
-ax.scatter(*nube_puntos.T)
-plt.show()
+np.save('recons_craneo3d.npy', recons)
+
+# ax = plt.axes(projection='3d')
+# ax.voxels(recons)
+# plt.show()
