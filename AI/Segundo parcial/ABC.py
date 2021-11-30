cb22e43903de19f9b0b864455836789bd5a13209
damadape <danielsapien@gmail.com>
2021-11-30 01:55:11 -0600

    ABC casi completo

---
+++ AI/Segundo parcial/ABC.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ABC():
+    def __init__(self, n_fuentes, x_min, x_max, dim):
+        self.n_fuentes = n_fuentes
+        self.x_min = x_max
+        self.x_max = x_min
+        self.dim = dim
+
+        self.fuentes = np.random.uniform(x_min, x_max, (n_fuentes,dim))
+        self.valores = np.zeros(n_fuentes)
+        self.explotados = np.zeros(n_fuentes, dtype=int)
+
+    def buscar(self, funcion, max_iter=100, limit_exploit=10):
+        self.f = funcion
+        self.valores = [funcion(fuente) for fuente in self.fuentes]
+        self.valores = np.array(self.valores)
+        self.mejor_valor = np.inf
+        self._registrar_mejor()
+        for _ in range(max_iter):
+            self._fase_empleadas()
+            self._fase_observadoras()
+            self._registrar_mejor()
+            self._fase_exploradoras(limit_exploit)
+        return self.mejor_sol, self.mejor_valor
+
+    def _registrar_mejor(self):
+        i = np.argmin(self.valores)
+        if self.valores[i] < self.mejor_valor:
+            self.mejor_valor = self.valores[i]
+            self.mejor_sol = self.fuentes[i]
+
+    def _probar_nueva_solucion(self, indice, nuevo_valor):
+        valor_busqueda = self.f(nuevo_valor)
+        if valor_busqueda < self.valores[indice]:
+            self.fuentes[indice] = nuevo_valor
+            self.valores[indice] = valor_busqueda
+            self.explotados[indice] = 0
+        else:
+            self.explotados[indice] += 1
+
+    def _fase_empleadas(self):
+        # Para buscar, cada fuente se acerca a otra fuente aleatoria
+        phi = np.random.uniform(-1,1, self.fuentes.shape)
+        paso = phi*(self.fuentes - np.random.permutation(self.fuentes))
+        busqueda = self.fuentes + paso
+        for i in range(self.n_fuentes):
+            self._probar_nueva_solucion(i, busqueda[i])
+
+    def _fase_observadoras(self):
+        probs = self.valores/self.valores.sum()
+        for _ in range(self.n_fuentes):
+            # Índice aleatorio en función de vector de probabilidades
+            i = np.random.choice(np.arange(self.n_fuentes), p=probs)
+            # Nueva solución
+            phi = np.random.uniform(-1, 1, self.n_fuentes)
+            paso = phi*(self.fuentes[i] - np.random.choice(self.fuentes))
+            busqueda = self.fuentes[i] + paso
+            self._probar_nueva_solucion(i, busqueda)
+
+    def _fase_exploradoras(self, limit_exploit):
+        #sobre_explotadas = self.explotados > limit_exploit
+        i = np.argmax(self.explotados)
+        if self.explotados[i] > limit_exploit:
+            self.fuentes[i] = np.random.uniform(self.x_min,
+                                                self.x_max,
+                                                self.dim)
+            self.valores[i] = self.f(self.fuentes[i])
+            self.explotados[i] = 0
+
+"""
+Pruebas
+"""
+if __name__ == '__main__':
+
+    def sphere(x):
+        #min @ (0, 0) = 0
+        return np.sum(x**2)
+
+    def eggholder(x):
+        #min @ (512, 404.2319) = -959.6407
+        term1 = -(x[1]+47) * np.sin(np.sqrt(abs(x[0]/2 + x[1]+47)))
+        term2 = -x[0] * np.sin(np.sqrt(abs(x[0] - (x[1]+47))))
+        return term1 + term2
+
+    colonia = ABC(n_fuentes=40, x_min=0, x_max=512, dim=2)
+    print(colonia.buscar(eggholder))
--- Img/Tercer parcial/reconst3d.py
+++ Img/Tercer parcial/reconst3d.py
@@ -15,11 +15,14 @@ idx_inicio = 104 # Número de primera imagen (voltea a derecha)
 idx_final = 150 # Número de última imagen (voltea a izquierda)
 n_imgs = idx_final - idx_inicio +1
 # Recorte de las imágenes
-y0, y1 = 270, 830
+y0, y1 = 280, 800
 x0, x1 = 10, 700
 recorte = np.s_[y0:y1, x0:x1]
-h_rebanada = 30
-rebanadas_radon = np.zeros(((y1-y0)//h_rebanada, n_imgs, x1-x0), dtype=np.float16)
+# h_rebanada = 30
+escala = 10
+rebanadas_radon = np.zeros(((y1-y0)//escala,
+                            n_imgs,
+                            (x1-x0)//escala), dtype=np.float16)
 
 for idx in range(0, n_imgs):
     # Cargar imagen
@@ -31,23 +34,24 @@ for idx in range(0, n_imgs):
     img_proc = rank.median(img_proc, morphology.disk(8))
     # Umbral de Otsu
     img_proc = util.img_as_ubyte(img_proc < filters.threshold_otsu(img_proc))
+    # Reducir tamaño
+    img_proc = transform.rescale(img_proc, 1/escala, anti_aliasing=True)
 
     # Sumar las rebanadas en este ciclo ahorra la necesidad
     # de tener todas las imágenes en memoria
-    for i in range(rebanadas_radon.shape[0]):
-        filas = np.s_[h_rebanada*i:h_rebanada*(i+1)]
-        rebanada = np.average(img_proc[filas], axis=0)
-        rebanadas_radon[i, idx] += rebanada
+    for f, fila in enumerate(img_proc):
+        rebanadas_radon[f, idx] += fila
 
 """
 Procesar el sinograma de cada rebanada
 """
-recons = np.zeros((y1-y0, x1-x0, x1-x0), dtype=np.byte)
+recons = np.zeros(((y1-y0)//escala,
+                   (x1-x0)//escala,
+                   (x1-x0)//escala), dtype=np.byte)
 for i, rebanada in enumerate(rebanadas_radon):
     proc = filtro_hamming(rebanada)
-    proc = inv_radon(proc) > 0.5
-    filas = np.s_[h_rebanada*i:h_rebanada*(i+1)]
-    recons[filas] = proc * np.ones((h_rebanada,1,1), dtype=np.byte)
-
+    proc = inv_radon(proc)
+    proc = proc > 0.5*np.max(proc)
+    recons[i] = proc
 
 np.save('recons_craneo3d.npy', recons)
